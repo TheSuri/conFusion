07658d1f3d33f613ef11c2ed45fe0c41c1344bb0
Gyan Suri <gsuri@usc.edu>
2017-09-06 01:45:50 -0700

    adding central config.js file

--- Server-rest-passport/config.js
+++ Server-rest-passport/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl' : 'mongodb://ec2-13-56-19-106.us-west-1.compute.amazonaws.com:27017/conFusion',
     'facebook': {
         clientID: '343318266116578',
         clientSecret: '6c84f3c43f1567018d1405a14318fe4d',
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
+        callbackURL: 'https://ec2-13-56-19-106.us-west-1.compute.amazonaws.com:3443/users/facebook/callback'
     }
   }
