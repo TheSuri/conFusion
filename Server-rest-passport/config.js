103e9c6fc9c7755bd921f38ec19c82d28672a2a4
Gyan Suri <gsuri@usc.edu>
2017-09-06 00:52:33 -0700

    refactored  minified & uglified for distribution

---
+++ Server-rest-passport/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '343318266116578',
+        clientSecret: '6c84f3c43f1567018d1405a14318fe4d',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+  }
