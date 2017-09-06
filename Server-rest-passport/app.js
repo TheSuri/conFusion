8bb515d96cc15d5a10badaeb3f097aea53161eff
Gyan Suri <gsuri@usc.edu>
2017-09-06 11:32:21 -0700

    Adding back cors

--- Server-rest-passport/app.js
+++ Server-rest-passport/app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cors= require('cors');
+
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
-//var cors= require('cors');
 // Secure traffic only
 
 var routes = require('./routes/index');
@@ -51,7 +52,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors()); 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- Server-rest-passport/config.js
+++ Server-rest-passport/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://ec2-13-56-19-106.us-west-1.compute.amazonaws.com:27017/conFusion',
+    'mongoUrl' : 'mongodb://admin2:password2@ec2-13-56-19-106.us-west-1.compute.amazonaws.com:27017/conFusion',
     'facebook': {
         clientID: '343318266116578',
         clientSecret: '6c84f3c43f1567018d1405a14318fe4d',
